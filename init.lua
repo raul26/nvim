f3813ab902fabe9f2ea20d469818078e2b44f18e
Raul Vargas <raulvargas@Rauls-MacBook-Pro.local>
2023-02-23 16:39:57 -0600

    First pass to nvim config.

---
+++ init.lua
@@ -0,0 +1,17 @@
+require("raulvargas.plugins-setup")
+require("raulvargas.core.options")
+require("raulvargas.core.keymaps")
+require("raulvargas.core.colorscheme")
+require("raulvargas.plugins.comment")
+require("raulvargas.plugins.lualine")
+require("raulvargas.plugins.telescope")
+require("raulvargas.plugins.mvim-cmp")
+require("raulvargas.plugins.autopairs")
+require("raulvargas.plugins.treesitter")
+require("raulvargas.plugins.gitsigns")
+--
+-- lsp
+require("raulvargas.plugins.lsp.mason")
+require("raulvargas.plugins.lsp.lspconfig")
+require("raulvargas.plugins.lsp.lspsaga")
+require("raulvargas.plugins.lsp.null-ls")
